aa27aebe76ccb3de3a0528cef874cdc9961c6d59
OXYEGN <129831825+PRADHAN474@users.noreply.github.com>
2023-08-20 11:28:39 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="❄ 𝙾𝚡𝚢𝚐𝚎𝙽 𝚁𝚘𝚋𝚘𝚃 ❄", url="https://t.me/Blossom_xmusic_bot"
+                text="❄ 𝙾𝚡𝚢𝚐𝚎𝙽 𝚁𝚘𝚋𝚘𝚃 ❄", url="https://t.me/oxgenxBot"
             )
         ],
      ]
